b336ec09946d3add5a6c00d9b561bd633a654821
Faarashed <alrashed.fr@gmail.com>
2020-06-17 20:51:44 +0300

    these are with the classwork

---
+++ iOSClasswork2/iOSClasswork2/ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,8 @@
+let family = ["ahmed", "rashed"]
+var evens = [2, 4, 6, 8, 10, 12]
+var grade = [86.3, 92.4, 90.0]
+var trueFalse = ["true", "true", "true", "false"]
+print(family[0], family [1])
+print(evens [0], evens[5])
+print(grade [0], grade [2])
+print(trueFalse [0], trueFalse[3])
---
+++ iOSClasswork2/iOSClasswork2/ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,12 @@
+var fruits = [ "mango", "orange", "potato", "peach"  ]
+print(fruits [0])
+print (fruits [3])
+fruits.append("strwaberry")
+print(fruits)
+fruits.remove(at: 2)
+print(fruits)
+fruits += ["banana", "watermelon", "pinapple", "kiwi"]
+print (fruits)
+fruits.removeAll()
+print(fruits)
+
